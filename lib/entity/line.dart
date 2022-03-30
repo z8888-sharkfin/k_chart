bb21c743fc83119954500e2ac2bcbb8920478147
zhangwei.abc <zhangwei.abc@bytedance.com>
2022-03-30 17:36:40 +0800

    linedraw

---
+++ lib/entity/line.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import,camel_case_types
+class Line {
+  int startIndex;
+  double startPrice;
+
+  int endIndex;
+  double endPrice;
+
+  Line({
+    required this.startIndex,
+    required this.startPrice,
+    required this.endIndex,
+    required this.endPrice,
+  });
+}
